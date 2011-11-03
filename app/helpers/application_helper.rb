94a2adfc141eaf624fea96ef41e6886dc8d90579
Enrique Marti <quique@elranchito.es>
2011-11-03 13:23:37 +0100

    Added logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return logo image in html
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample Application"
