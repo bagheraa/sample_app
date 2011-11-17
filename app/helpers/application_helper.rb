cfc2fc7496e666e62af298bbe13fab8b2833d371
Enrique Marti <quique@elranchito.es>
2011-11-17 16:58:10 +0100

    Restored repo

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,18 @@
+module ApplicationHelper
+
+  # Return logo image in html
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
+  # Return title on a per-page basis
+  def title
+    base_title = "Ruby on Rails Tutorial Sample Application"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+end
